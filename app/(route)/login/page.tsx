870860697d33d75c3f4b1729996dbe792257c7ce
Jayden Oerson <jaydenoerson95@gmail.com>
2025-02-09 13:59:04 +0200

    Adding styling to register and signin pages.

--- app/(route)/login/page.tsx
+++ app/(route)/login/page.tsx
@@ -1,63 +1,35 @@
-"use client";
-import { useState } from "react";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { LoginForm } from "@/app/_components/LoginForm"
 
-export default function Login() {
-  const [error, setError] = useState("");
-  const router = useRouter();
+import { GalleryVerticalEnd } from "lucide-react"
+import Image from "next/image"
 
-  const handleSubmit = async (formData: FormData) => {
-    const res = await signIn("credentials", {
-      email: formData.get("email"),
-      password: formData.get("password"),
-      redirect: false,
-    });
-    if (res?.error) {
-      setError(res.error as string);
-    }
-    if (res?.ok) {
-      return router.push("/");
-    }
-  };
-
+export default function LoginPage() {
   return (
-    <section className="w-full h-screen flex items-center justify-center">
-      <form
-        className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
-        border border-solid border-black bg-white rounded text-black"
-        action={handleSubmit}
-      >
-        {error && <div className="text-black">{error}</div>}
-        <h1 className="mb-5 w-full text-2xl font-bold">Sign In</h1>
-        <label className="w-full text-sm">Email</label>
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full h-8 border border-solid border-black rounded p-2"
-          name="email"
-        />
-        <label className="w-full text-sm">Password</label>
-        <div className="flex w-full">
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full h-8 border border-solid border-black rounded p-2"
-            name="password"
-          />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Acme Inc.
+          </a>
         </div>
-        <button className="w-full rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-          Sign In
-        </button>
-
-        <Link
-          href="/register"
-          className="text-sm text-[#888] transition duration-150 ease hover:text-black"
-        >
-          Don&apos;t have an account?
-        </Link>
-      </form>
-    </section>
-  );
-};
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <Image
+          src="/login.jpg"
+          alt="Image"
+          className="absolute origin-center inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          height={1200}
+          width={1200}
+        />
+      </div>
+    </div>
+  )
+}
